712af48f2e707c024379f1abc94284016e2ba6b3
nurhawanaa <nurhawananasution14@gmail.com>
2025-06-25 17:41:15 +0700

    mengirimkan data ke view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home') ;
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Nurhawana Nasution",
+        "email" => "nurhawananasution14@gmail.com",
+        "image" => "nnur.jpeg"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts') ;
 });
